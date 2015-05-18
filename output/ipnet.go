4903231b71ef5eaf67298b13b529312074628862
João Ricardo Petreli Jorge <joao.petrelli@gmail.com>
2015-05-18 17:55:53 -0300

    IPNetwork output and its test. See #4.

---
+++ output/ipnet.go
@@ -0,0 +1,49 @@
+package output
+
+import (
+	"io"
+	"text/template"
+	"time"
+
+	"github.com/registrobr/rdap-client/protocol"
+)
+
+type IPNetwork struct {
+	IPNetwork *protocol.IPNetwork
+
+	CreatedAt string
+	UpdatedAt string
+
+	ContactsInfos []ContactInfo
+}
+
+func (i *IPNetwork) setDates() {
+	for _, e := range i.IPNetwork.Events {
+		date := e.Date.Format(time.RFC3339)
+
+		switch e.Action {
+		case protocol.EventActionRegistration:
+			i.CreatedAt = date
+		case protocol.EventActionLastChanged:
+			i.UpdatedAt = date
+		}
+	}
+}
+
+func (i *IPNetwork) ToText(wr io.Writer) error {
+	i.setDates()
+
+	i.ContactsInfos = make([]ContactInfo, 0, len(i.IPNetwork.Entities))
+	for _, entity := range i.IPNetwork.Entities {
+		var c ContactInfo
+		c.setContact(entity)
+		i.ContactsInfos = append(i.ContactsInfos, c)
+	}
+
+	t, err := template.New("ipnetwork template").Parse(ipnetTmpl)
+	if err != nil {
+		return err
+	}
+
+	return t.Execute(wr, i)
+}
---
+++ output/ipnet_test.go
@@ -0,0 +1,100 @@
+package output
+
+import (
+	"testing"
+	"time"
+
+	"github.com/registrobr/rdap-client/protocol"
+)
+
+var TestIPNetworkToTextOutput = `inetnum:       (IPNetwork)
+aut-num:       ip_123456-NICBR
+abuse-c:       (handle)
+owner:         
+ownerid:       (CPF/CNPJ)
+responsible:   
+address:     
+address:     
+country:       BR
+phone:       
+start-address: 200.160.3.0
+end-address:   200.160.3.255
+ip-version:    v4
+type:          DIRECT ALLOCATION
+parent-handle: ip_1-NICBR
+status:        [active]
+owner-c:     
+tech-c:      
+inetrev:     
+nserver:     
+nsstat:      
+nslastaa:    
+created:     2015-03-01T12:00:00Z
+changed:     2015-03-10T14:00:00Z
+
+nic-hdl-br: XXXX
+person: Joe User
+e-mail: joe.user@example.com
+address:  Av Naçoes Unidas 11541 7 andar Sao Paulo SP 04578-000 BR
+phone: tel:+55-11-5509-3506;ext=3506
+created: 2015-03-01T12:00:00Z
+changed: 2015-03-10T14:00:00Z
+`
+
+func TestIPNetworkToText(t *testing.T) {
+	ipNetworkResponse := protocol.IPNetwork{
+		ObjectClassName: "ip network",
+		Handle:          "ip_123456-NICBR",
+		StartAddress:    "200.160.3.0",
+		EndAddress:      "200.160.3.255",
+		IPVersion:       "v4",
+		Name:            "",
+		Type:            "DIRECT ALLOCATION",
+		Country:         "BR",
+		ParentHandle:    "ip_1-NICBR",
+		Status:          []string{"active"},
+		Events: []protocol.Event{
+			{Action: protocol.EventActionRegistration, Actor: "", Date: time.Date(2015, 03, 01, 12, 00, 00, 00, time.UTC)},
+			{Action: protocol.EventActionLastChanged, Actor: "", Date: time.Date(2015, 03, 10, 14, 00, 00, 00, time.UTC)},
+		},
+		Entities: []protocol.Entity{
+			{
+				ObjectClassName: "entity",
+				Handle:          "XXXX",
+				VCardArray: []interface{}{
+					"vcard",
+					[]interface{}{
+						[]interface{}{"version", struct{}{}, "text", "4.0"},
+						[]interface{}{"fn", struct{}{}, "text", "Joe User"},
+						[]interface{}{"kind", struct{}{}, "text", "individual"},
+						[]interface{}{"email", struct{ Type string }{Type: "work"}, "text", "joe.user@example.com"},
+						[]interface{}{"lang", struct{ Pref string }{Pref: "1"}, "language-tag", "pt"},
+						[]interface{}{"adr", struct{ Type string }{Type: "work"}, "text",
+							[]string{
+								"Av Naçoes Unidas", "11541", "7 andar", "Sao Paulo", "SP", "04578-000", "BR",
+							},
+						},
+						[]interface{}{"tel", struct{ Type string }{Type: "work"}, "uri", "tel:+55-11-5509-3506;ext=3506"},
+					},
+				},
+				Events: []protocol.Event{
+					protocol.Event{Action: protocol.EventActionRegistration, Actor: "", Date: time.Date(2015, 03, 01, 12, 00, 00, 00, time.UTC)},
+					protocol.Event{Action: protocol.EventActionLastChanged, Actor: "", Date: time.Date(2015, 03, 10, 14, 00, 00, 00, time.UTC)},
+				},
+			},
+		},
+	}
+
+	ipNetworkOutput := IPNetwork{IPNetwork: &ipNetworkResponse}
+
+	var w WriterMock
+	if err := ipNetworkOutput.ToText(&w); err != nil {
+		t.Fatal(err)
+	}
+
+	if string(w.Content) != TestIPNetworkToTextOutput {
+		t.Error("Wrong output")
+		t.Log("'", (string(w.Content)), "'")
+		return
+	}
+}
