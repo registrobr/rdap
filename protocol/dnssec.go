b72725fccb4c834e3bcd7bb80227c2d89bc82582
Rafael Dantas Justo <rafael@registro.br>
2015-06-23 15:59:11 -0300

    Add LastCheck and Last OK to nameserver and DS objects Extension document of NIC.br RDAP will be published in IANA.

--- protocol/dnssec.go
+++ protocol/dnssec.go
@@ -1,12 +1,16 @@
 package protocol
 
+import "time"
+
 type DS struct {
-	KeyTag     int     `json:"keyTag"`
-	Algorithm  int     `json:"algorithm"`
-	Digest     string  `json:"digest"`
-	DigestType int     `json:"digestType"`
-	Events     []Event `json:"events,omitempty"`
-	DSStatus   string  `json:"nicbr_status,omitempty"`
+	KeyTag      int       `json:"keyTag"`
+	Algorithm   int       `json:"algorithm"`
+	Digest      string    `json:"digest"`
+	DigestType  int       `json:"digestType"`
+	Events      []Event   `json:"events,omitempty"`
+	DSStatus    string    `json:"nicbr_status,omitempty"`
+	LastCheckAt time.Time `json:"nicbr_last_check,omitempty"`
+	LastOKAt    time.Time `json:"nicbr_last_ok,omitempty"`
 }
 
 type SecureDNS struct {
