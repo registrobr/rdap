8d3922622cdd2b841fa6fd2fe21fcafff2bb3c3d
Rafael Dantas Justo <rafael@registro.br>
2015-08-07 10:09:31 -0300

    Major refactory and documentation

--- handler.go
+++ handler.go
@@ -13,19 +13,48 @@ import (
 )
 
 var (
-	isFQDN          = regexp.MustCompile(`^(([[:alnum:]](([[:alnum:]]|\-){0,61}[[:alnum:]])?\.)*[[:alnum:]](([[:alnum:]]|\-){0,61}[[:alnum:]])?)?(\.)?$`)
+	isFQDN = regexp.MustCompile(`^((([a-z0-9][a-z0-9\-]*[a-z0-9])|[a-z0-9]+)\.)*([a-z]+|xn\-\-[a-z0-9]+)\.?$`)
+
+	// ErrInvalidQuery reports an query with an invalid format
 	ErrInvalidQuery = errors.New("invalid query")
 )
 
-type Handler struct {
-	URIs          []string
-	HTTPClient    *http.Client
-	Bootstrap     *Bootstrap
-	XForwardedFor string
+// BootstrapClient is a client with the bootstrap logic
+type BootstrapClient interface {
+	Query(object string) (interface{}, error)
+	Domain(object string) (*protocol.Domain, error)
+	ASN(object string) (*protocol.AS, error)
+	Entity(object string) (*protocol.Entity, error)
+	IPNetwork(object string) (*protocol.IPNetwork, error)
+	IP(object string) (*protocol.IPNetwork, error)
+	SetXForwardedFor(addr string)
 }
 
-func (h *Handler) Query(object string) (interface{}, error) {
-	generic := genericQuerier{handler: h}
+// NewBootstrapClient returns a client with bootstrap capabilities. You can
+// inject a HTTP client and a bootstrap URL starting point, if no HTTP client is
+// defined, a new one is created, and if no bootstrap URL is given, the
+// RDAPBootstrap constant will be used
+var NewBootstrapClient = func(uris []string, httpClient *http.Client, bootstrap string) BootstrapClient {
+	b := bootstrapClient{
+		uris:      uris,
+		client:    NewClient(uris, httpClient),
+		bootstrap: NewBootstrap(httpClient),
+	}
+
+	if bootstrap != "" {
+		b.bootstrap.Bootstrap = bootstrap
+	}
+	return &b
+}
+
+type bootstrapClient struct {
+	uris      []string
+	client    Client
+	bootstrap *Bootstrap
+}
+
+func (b *bootstrapClient) Query(object string) (interface{}, error) {
+	generic := genericQuerier{handler: b}
 	handlers := []func(object string) (interface{}, error){
 		generic.ASN,
 		generic.IP,
@@ -50,140 +79,123 @@ func (h *Handler) Query(object string) (interface{}, error) {
 	return nil, ErrInvalidQuery
 }
 
-func (h *Handler) ASN(object string) (*protocol.AS, error) {
+func (b *bootstrapClient) Domain(object string) (*protocol.Domain, error) {
+	if !isFQDN.MatchString(object) || !strings.Contains(object, ".") {
+		return nil, ErrInvalidQuery
+	}
+
+	uris := b.uris
+
+	if b.bootstrap != nil {
+		var err error
+		uris, err = b.bootstrap.Domain(object)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	b.client.SetURIs(uris)
+	return b.client.Domain(object)
+}
+
+func (b *bootstrapClient) ASN(object string) (*protocol.AS, error) {
 	asn, err := strconv.ParseUint(object, 10, 32)
 
 	if err != nil {
 		return nil, ErrInvalidQuery
 	}
 
-	uris := h.URIs
+	uris := b.uris
 
-	if h.Bootstrap != nil {
+	if b.bootstrap != nil {
 		var err error
-		uris, err = h.Bootstrap.ASN(asn)
+		uris, err = b.bootstrap.ASN(asn)
 
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	cli := NewClient(uris, h.HTTPClient)
-	if h.XForwardedFor != "" {
-		cli.XForwardedFor = h.XForwardedFor
-	}
-
-	return cli.ASN(asn)
+	b.client.SetURIs(uris)
+	return b.client.ASN(asn)
 }
 
-func (h *Handler) Entity(object string) (*protocol.Entity, error) {
+func (b *bootstrapClient) Entity(object string) (*protocol.Entity, error) {
 	// Note that there is no bootstrap for entity, see [1]
 	// [1] - https://tools.ietf.org/html/rfc7484#section-6
-
-	cli := NewClient(h.URIs, h.HTTPClient)
-	if h.XForwardedFor != "" {
-		cli.XForwardedFor = h.XForwardedFor
-	}
-
-	return cli.Entity(object)
+	b.client.SetURIs(b.uris)
+	return b.client.Entity(object)
 }
 
-func (h *Handler) IPNetwork(object string) (*protocol.IPNetwork, error) {
+func (b *bootstrapClient) IPNetwork(object string) (*protocol.IPNetwork, error) {
 	_, cidr, err := net.ParseCIDR(object)
 
 	if err != nil {
 		return nil, ErrInvalidQuery
 	}
 
-	uris := h.URIs
+	uris := b.uris
 
-	if h.Bootstrap != nil {
+	if b.bootstrap != nil {
 		var err error
-		uris, err = h.Bootstrap.IPNetwork(cidr)
+		uris, err = b.bootstrap.IPNetwork(cidr)
 
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	cli := NewClient(uris, h.HTTPClient)
-	if h.XForwardedFor != "" {
-		cli.XForwardedFor = h.XForwardedFor
-	}
-
-	return cli.IPNetwork(cidr)
+	b.client.SetURIs(uris)
+	return b.client.IPNetwork(cidr)
 }
 
-func (h *Handler) IP(object string) (*protocol.IPNetwork, error) {
+func (b *bootstrapClient) IP(object string) (*protocol.IPNetwork, error) {
 	ip := net.ParseIP(object)
 
 	if ip == nil {
 		return nil, ErrInvalidQuery
 	}
 
-	uris := h.URIs
+	uris := b.uris
 
-	if h.Bootstrap != nil {
+	if b.bootstrap != nil {
 		var err error
-		uris, err = h.Bootstrap.IP(ip)
+		uris, err = b.bootstrap.IP(ip)
 
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	cli := NewClient(uris, h.HTTPClient)
-	if h.XForwardedFor != "" {
-		cli.XForwardedFor = h.XForwardedFor
-	}
-
-	return cli.IP(ip)
+	b.client.SetURIs(uris)
+	return b.client.IP(ip)
 }
 
-func (h *Handler) Domain(object string) (*protocol.Domain, error) {
-	if !isFQDN.MatchString(object) || !strings.Contains(object, ".") {
-		return nil, ErrInvalidQuery
-	}
-
-	uris := h.URIs
-
-	if h.Bootstrap != nil {
-		var err error
-		uris, err = h.Bootstrap.Domain(object)
-
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	cli := NewClient(uris, h.HTTPClient)
-	if h.XForwardedFor != "" {
-		cli.XForwardedFor = h.XForwardedFor
-	}
-
-	return cli.Domain(object)
+func (b *bootstrapClient) SetXForwardedFor(addr string) {
+	b.client.SetXForwardedFor(addr)
 }
 
 type genericQuerier struct {
-	handler *Handler
+	handler BootstrapClient
 }
 
-func (h *genericQuerier) ASN(object string) (interface{}, error) {
-	return h.handler.ASN(object)
+func (g *genericQuerier) ASN(object string) (interface{}, error) {
+	return g.handler.ASN(object)
 }
 
-func (h *genericQuerier) Entity(object string) (interface{}, error) {
-	return h.handler.Entity(object)
+func (g *genericQuerier) Entity(object string) (interface{}, error) {
+	return g.handler.Entity(object)
 }
 
-func (h *genericQuerier) IPNetwork(object string) (interface{}, error) {
-	return h.handler.IPNetwork(object)
+func (g *genericQuerier) IPNetwork(object string) (interface{}, error) {
+	return g.handler.IPNetwork(object)
 }
 
-func (h *genericQuerier) IP(object string) (interface{}, error) {
-	return h.handler.IP(object)
+func (g *genericQuerier) IP(object string) (interface{}, error) {
+	return g.handler.IP(object)
 }
 
-func (h *genericQuerier) Domain(object string) (interface{}, error) {
-	return h.handler.Domain(object)
+func (g *genericQuerier) Domain(object string) (interface{}, error) {
+	return g.handler.Domain(object)
 }
