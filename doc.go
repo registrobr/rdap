66f5f154ec78820e3fc6ccbfa9e5dd69186f6968
Rafael Dantas Justo <rafael@registro.br>
2015-08-07 14:01:38 -0300

    Add documentation file

---
+++ doc.go
@@ -0,0 +1,5 @@
+// RDAP (Registration Data Access Protocol) is a library to be used in
+// clients and servers to make the life easier when building requests and
+// responses. You will find all RDAP protocol types in the protocol package and
+// can use the clients to build your own client tool.
+package rdap
