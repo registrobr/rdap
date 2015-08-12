572021e1ce74e1a39ac5dc023e76723d83bce9cf
Rafael Dantas Justo <rafael@registro.br>
2015-08-12 14:15:38 -0300

    Make ZoneSigned optional

--- protocol/dnssec.go
+++ protocol/dnssec.go
@@ -16,7 +16,11 @@ type DS struct {
 
 // SecureDNS describes the secureDNS as it is in RFC 7483, section 5.3
 type SecureDNS struct {
-	ZoneSigned       bool `json:"zoneSigned"`
-	DelegationSigned bool `json:"delegationSigned"`
-	DSData           []DS `json:"dsData,omitempty"`
+	// ZoneSigned does not make too much sense for us to use
+	// it, so we need to use a pointer to hide it with omitempty. Maybe the
+	// real use for it is for TLDs that publish the DS records
+	// without signing it, but its not clear in RFC 7483, section 5.3
+	ZoneSigned       *bool `json:"zoneSigned,omitempty"`
+	DelegationSigned bool  `json:"delegationSigned"`
+	DSData           []DS  `json:"dsData,omitempty"`
 }
