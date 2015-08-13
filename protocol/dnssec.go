2962736ae091ebdd0dcd14ed041282ab88761ef6
Rafael Dantas Justo <rafael@registro.br>
2015-08-13 10:53:41 -0300

    Changing some NIC.br extensions to WG proposals

--- protocol/dnssec.go
+++ protocol/dnssec.go
@@ -1,17 +1,12 @@
 package protocol
 
-import "time"
-
 // DS describes the dsData as it is in RFC 7483, section 5.3
 type DS struct {
-	KeyTag      int       `json:"keyTag"`
-	Algorithm   int       `json:"algorithm"`
-	Digest      string    `json:"digest"`
-	DigestType  int       `json:"digestType"`
-	Events      []Event   `json:"events,omitempty"`
-	DSStatus    string    `json:"nicbr_status,omitempty"`
-	LastCheckAt time.Time `json:"nicbr_lastCheck,omitempty"`
-	LastOKAt    time.Time `json:"nicbr_lastOK,omitempty"`
+	KeyTag     int     `json:"keyTag"`
+	Algorithm  int     `json:"algorithm"`
+	Digest     string  `json:"digest"`
+	DigestType int     `json:"digestType"`
+	Events     []Event `json:"events,omitempty"`
 }
 
 // SecureDNS describes the secureDNS as it is in RFC 7483, section 5.3
